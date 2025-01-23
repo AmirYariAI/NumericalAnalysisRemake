151fa95a76423f09ffe7b9661f71070cb37fc75d
am-linux <yarya524@gmail.com>
2025-01-24 03:22:15 +0330

    refactoring design & debugging debug table
    Signed-off-by: am-linux <yarya524@gmail.com>

--- EquationSolvers.py
+++ EquationSolvers.py
@@ -3,77 +3,52 @@ from DebugAssistant import debug_status
 from typing import  List
 
 
-def debug_table_header(n:int):
-    word_space = (MAX_DIGITS +5 )
-    row_space = 14 + n * (word_space + 1) + max(word_space,12) +  + 7
+class EquationSolvers:
 
-    print("-( Gauss–Seidel Table )- ".center(row_space))
-    print("iteration \\ X ",end="|")
-    for i in range(n):
-        print(f"X{i}".center(word_space),end='|')
+    @staticmethod
+    def debug_table_header(n: int):
+        word_space = (MAX_DIGITS + 5)
+        row_space = 14 + n * (word_space + 1) + max(word_space, 12) + + 7
 
-    print('mean error'.center(max(word_space,12)),' note ',sep='|')
+        print("-( Gauss–Seidel Table )- ".center(row_space))
+        print("iteration \\ X ", end="|")
+        for i in range(n):
+            print(f"X{i}".center(word_space), end='|')
 
-def debug_tabel_row(i:int,x_values : List[float] , error : float,note:str=""):
+        print('mean error'.center(max(word_space, 12)), ' note ', sep='|')
 
-    print(f" iteration {i}  ", end="|")
-    n = len(x_values)
-    word_space = (MAX_DIGITS + 5)
-    for i in range(n):
-        print(f"{x_values[i]}".center(word_space),end='|')
+    @staticmethod
+    def debug_tabel_row(i: int, x_values: List[float], error: float, note: str = ""):
 
-    print(f" {error}".center(max(word_space,12)) , end='|')
-    print(" " + note)
+        print(f"iteration {i}".center(14), end="|")
+        n = len(x_values)
+        word_space = (MAX_DIGITS + 5)
+        for i in range(n):
+            print(f"{x_values[i]}".center(word_space), end='|')
 
-def gauss_seidel(a:List[List[float]] ,
+        print(f" {error}".center(max(word_space, 12)), end='|')
+        print(" " + note)
+
+    @staticmethod
+    def gauss_seidel(a:List[List[float]] ,
                  starter_x : List[float] ,
                  b : List[float],
                  max_iteration:int = 20,
                  debug_mode:str = "auto") -> List[float]:
 
-    debug = debug_status(debug_mode)
+        debug = debug_status(debug_mode)
 
-    n = len(starter_x)
+        n = len(starter_x)
 
-    if len(a) != n or len(a[0]) != n or len(b) != n:
-        raise ValueError("input in invalid")
+        if len(a) != n or len(a[0]) != n or len(b) != n:
+            raise ValueError("input in invalid")
 
-    x : List[float] = starter_x.copy()
+        x : List[float] = starter_x.copy()
 
-    if debug:
-        debug_table_header(n)
+        if debug:
+            EquationSolvers.debug_table_header(n)
 
-    epsilon = 10 ** (-MAX_DIGITS)
-
-    error = 0
-    for i in range(n):
-        eq_error = 0
-
-        for j in range(n):
-            eq_error += round(a[i][j] * x[j], MAX_DIGITS)
-
-        eq_error = round(eq_error - b[i])
-        error = round(error + abs(eq_error), MAX_DIGITS)
-
-    error = round(error / n, MAX_DIGITS)
-
-    iteration = 0
-
-    while error > epsilon and iteration < max_iteration:
-
-        if debug :
-            debug_tabel_row(i=iteration,x_values=x,error=error)
-
-        for i in range(n):
-            eq_value = b[i]
-            for j in range(n):
-                if i == j:
-                    continue
-
-                eq_value -= round(a[i][j] * x[j], MAX_DIGITS)
-
-            factor = round(1/a[i][i],MAX_DIGITS)
-            x[i] = round(factor * eq_value,MAX_DIGITS)
+        epsilon = 10 ** (-MAX_DIGITS)
 
         error = 0
         for i in range(n):
@@ -87,14 +62,44 @@ def gauss_seidel(a:List[List[float]] ,
 
         error = round(error / n, MAX_DIGITS)
 
-        iteration += 1
+        iteration = 0
 
-    if debug:
-        note = "" if iteration < max_iteration else "max_iteration reached"
+        while error > epsilon and iteration < max_iteration:
 
-        debug_tabel_row(i=iteration, x_values=x, error=error,note=note)
+            if debug :
+                EquationSolvers.debug_tabel_row(i=iteration,x_values=x,error=error)
 
-    return x
+            for i in range(n):
+                eq_value = b[i]
+                for j in range(n):
+                    if i == j:
+                        continue
+
+                    eq_value -= round(a[i][j] * x[j], MAX_DIGITS)
+
+                factor = round(1/a[i][i],MAX_DIGITS)
+                x[i] = round(factor * eq_value,MAX_DIGITS)
+
+            error = 0
+            for i in range(n):
+                eq_error = 0
+
+                for j in range(n):
+                    eq_error += round(a[i][j] * x[j], MAX_DIGITS)
+
+                eq_error = round(eq_error - b[i])
+                error = round(error + abs(eq_error), MAX_DIGITS)
+
+            error = round(error / n, MAX_DIGITS)
+
+            iteration += 1
+
+        if debug:
+            note = "" if iteration < max_iteration else "max_iteration reached"
+
+            EquationSolvers.debug_tabel_row(i=iteration, x_values=x, error=error,note=note)
+
+        return x
 
 
 if __name__ == "__main__":
@@ -110,5 +115,5 @@ if __name__ == "__main__":
     b = [4 , 7 , 3]
     x = [0 , 0 , 0]
 
-    x=gauss_seidel(a,x,b)
+    x=EquationSolvers.gauss_seidel(a,x,b)
     print(x)
